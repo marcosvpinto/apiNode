9744e20503e9bc6854435d67e1a74636369fb3c3
mvpdev@gmail.com <v93i39u8>
2018-06-26 16:50:44 -0300

    adicionando arquivos

---
+++ app/routes/note_routes.js
@@ -0,0 +1,20 @@
+// routes/note_routes.js
+
+module.exports = function (app, db) {
+    const collection =
+        app.post('/notes', (req, res) => {
+            const note = {
+                text: req.body.body,
+                title: req.body.title
+            };
+            db.collection('notes').insert(note, (err, result) => {
+                if (err) {
+                    res.send({
+                        'error': 'An error has occured'
+                    });
+                } else {
+                    res.send(result.ops[0]);
+                }
+            });
+        });
+};
---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: 'mongodb://api:apinode1@ds217921.mlab.com:17921/apinode'
+}
